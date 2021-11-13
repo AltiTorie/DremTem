f9e69328824909f00c80fb359237c9642bea91b8
kamilagalka <contactkamilagalka@gmail.com>
2021-11-13 02:28:20 +0100

    Add Device Config Screen draft

--- AwesomeProject/screens/DeviceConfigScreen.js
+++ AwesomeProject/screens/DeviceConfigScreen.js
@@ -1,15 +1,10 @@
 import React, {useState} from 'react';
-import {
-  Text,
-  View,
-  StyleSheet,
-  Switch,
-  ToastAndroid,
-  TextInput,
-} from 'react-native';
 import AppButton from '../components/Button_main';
 import SelectDropdown from 'react-native-select-dropdown';
 import DeviceStackNavigator from '../navigation/DeviceStackNavigator';
+import {Text, View, StyleSheet, Switch} from 'react-native';
+import AppButton from '../components/Button';
+import SelectDropdown from 'react-native-select-dropdown';
 
 const DeviceConfigScreen = props => {
   const getSensorsNamesList = sensorsData => {
